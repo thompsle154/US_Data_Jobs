7bf4f539fc1d0ef2c1d5c6821c5f896f4552219d
Leanne Thompson <78496051+thompsle154@users.noreply.github.com>
2021-08-30 13:51:30 -0600

    renamed repo

---
+++ config.py
@@ -0,0 +1,11 @@
+# connection parameters
+user = "postgres"
+password = "password"
+host = "localhost"
+database = "jobs_db"
+param_dic = {
+    "host" : host,
+    "database" : database,
+    "user" : user,
+    "password" : password
+}
---
+++ query_tables.sql
@@ -0,0 +1,3 @@
+select *
+from data_scientist
+
